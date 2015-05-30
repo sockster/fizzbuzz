2a1e55633856d52d554577e19eea961845097412
Sailor Socks <socksster@gmail.com>
2015-05-30 14:26:54 -0400

    final explain

--- fizzbuzz_04.py
+++ fizzbuzz_04.py
@@ -10,3 +10,21 @@ if sys.argv[1]
 
 # and now this line is created to see if
 # git & github recognize this new version
+
+"""
+2:11pm - 05/30/15 - FINALLY GOT GitHub (to work)!!!
+pebcak ... 
+first, changed default (per msg stating change for GitHub 2.0) because
+	"To squelch this message and maintain the current behavior after the default changes, use:
+  git config --global push.default matching
+
+To squelch this message and adopt the new behavior now, use:
+  git config --global push.default simple
+(I used "simple")
+
+then set upstream master because  "The current branch master has no upstream branch":
+	git push --set-upstream origin master
+
+"""
+
+
