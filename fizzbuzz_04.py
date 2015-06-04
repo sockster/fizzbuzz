c6ee373f227a124cbdfaa8c40a7d5b11e1c896c4
Sailor Socks <socksster@gmail.com>
2015-06-03 20:01:10 -0400

    fizzbuzz using sys.argv

--- fizzbuzz_04.py
+++ fizzbuzz_04.py
@@ -1,10 +1,54 @@
+# (text homework)
+# run FizzBuzz sequence from either user-supplied number (variable) or from 1
+
+
+# if sys.argv has more arguments than 1 (sys.argv[0] - filename), save to var "start"
+# else, use supplied argument (stored in sys.argv[1], save to var "start"
+# convert (var in) start to integer
+# print (var) start
+# while start is less than 101, do fizzbuzz routine beginning from (var) start 
+
+
 import sys
 
+def fizzy():
+	if len(sys.argv) >= 2:
+		start = sys.argv[1]
+	else:
+		start = 1
+	start = int(start)
+	print start
+	print " "
+	while start < 101:
+		if start % 3 == 0 and start % 5 == 0:
+			print "FizzBuzz"
+		elif start % 5 == 0:
+			print "Buzz"
+		elif start % 3 == 0:
+			print "Fizz"
+		else:
+			print start
+		start += 1
 
-# if sys.var[1] is empty (user has not called a var), use int(1)
-# if sys.var[1] returns a #, convert to int and begin fizzbuzz routine from there
+fizzy()
 
-if sys.argv[1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this is my latest github attempt
 
