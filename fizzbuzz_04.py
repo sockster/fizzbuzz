1bd1cbaf6d1946d45134ed8468fec91e6ba89fe5
Sailor Socks <socksster@gmail.com>
2015-06-03 20:13:59 -0400

    fizzbuzz using raw_input

--- fizzbuzz_04.py
+++ fizzbuzz_04.py
@@ -9,6 +9,8 @@
 # while start is less than 101, do fizzbuzz routine beginning from (var) start 
 
 
+
+"""
 import sys
 
 def fizzy():
@@ -31,41 +33,29 @@ def fizzy():
 		start += 1
 
 fizzy()
+"""
+
+# run FizzBuzz sequence beginning from user-supplied number using raw_input
+
+def fizzbee():
+	start = raw_input("Where do you want me to start FizzBuzzing from? > ")
+	start = int(start)
+
+	while start < 101:
+		if start % 3 == 0 and start % 5 == 0:
+			print "FizzBuzz"
+		elif start % 5 == 0:
+			print "Buzz"
+		elif start % 3 == 0:
+			print "Fizz"
+		else:
+			print start
+		start += 1
+
+
+fizzbee()
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# this is my latest github attempt
-
-# and now this line is created to see if
-# git & github recognize this new version
-
-
-#	2:11pm - 05/30/15 - FINALLY GOT GitHub (to work)!!!
-#	pebcak ... 
-#	first, changed default (per msg stating change for GitHub 2.0) because
-#		"To squelch this message and maintain the current behavior after the default changes, use:
-#	  git config --global push.default matching
-
-#	To squelch this message and adopt the new behavior now, use:
-#	  git config --global push.default simple
-#	(I used "simple")
-
-#	then set upstream master because  "The current branch master has no upstream branch":
-#		git push --set-upstream origin master
 
